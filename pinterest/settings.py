dafbae2a888ab7ef11b99db141d0fb8604b4261d
josephsssss <josephsssss@hotmail.com>
2021-12-09 13:55:10 +0900

    profile authenticator added

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -135,9 +135,9 @@ STATICFILES_DIRS = [
 ]
 
 
-STATIC_URL = 'media/'
+MEDIA_URL = 'media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
